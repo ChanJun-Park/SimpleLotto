483882c5823f4063798cf4032e6780c0e3bb6317
박찬준 <chanjun.park@navercorp.com>
2022-03-07 07:11:14 +0900

    model 클래스 추가

---
+++ app/src/main/java/com/jingom/simplelotto/database/model/DatabaseLottoResult.kt
@@ -0,0 +1,53 @@
+package com.jingom.simplelotto.database.model
+
+import androidx.annotation.VisibleForTesting
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.jingom.simplelotto.screens.model.LottoResult
+
+@VisibleForTesting
+const val INVALID = -1
+
+@Entity(tableName = "lotto_result")
+data class DatabaseLottoResult(
+	@PrimaryKey
+	@ColumnInfo(name = "lottery_no")
+	val lotteryNo: Int = INVALID,
+
+	val day: String = "",
+
+	@ColumnInfo(name = "total_sell_amount")
+	val totalSellAmount: Long = INVALID.toLong(),
+
+	@ColumnInfo(name = "first_win_amount")
+	val firstWinAmount: Long = INVALID.toLong(),
+
+	@ColumnInfo(name = "first_prize_winner_count")
+	val firstPrizeWinnerCount: Int = INVALID,
+
+	@ColumnInfo(name = "first_accumulated_amount")
+	val firstAccumulatedAmount: Long = INVALID.toLong(),
+
+	val no1: Int = INVALID,
+	val no2: Int = INVALID,
+	val no3: Int = INVALID,
+	val no4: Int = INVALID,
+	val no5: Int = INVALID,
+	val no6: Int = INVALID,
+
+	@ColumnInfo(name = "bonus_no")
+	val bonusNo: Int = INVALID
+)
+
+fun DatabaseLottoResult.toUiModel() = LottoResult(
+	no1 = no1,
+	no2 = no2,
+	no3 = no3,
+	no4 = no4,
+	no5 = no5,
+	no6 = no6,
+	bonusNo = bonusNo,
+	lotteryNo = lotteryNo,
+	day = day
+)
---
+++ app/src/main/java/com/jingom/simplelotto/network/model/NetworkLottoResult.kt
@@ -0,0 +1,58 @@
+package com.jingom.simplelotto.network.model
+
+import com.jingom.simplelotto.database.model.DatabaseLottoResult
+import com.squareup.moshi.Json
+
+private const val INVALID = -1
+const val SUCCESS_RESULT_STRING = "success"
+
+data class NetworkLottoResult(
+	@Json(name = "returnValue")
+	val apiResult: String,
+	@Json(name = "drwNoDate")
+	val dayOfLottery: String?,
+	@Json(name = "totSellamnt")
+	val totalSellAmount: Long = INVALID.toLong(),
+	@Json(name = "firstWinamnt")
+	val firstWinAmount: Long = INVALID.toLong(),
+	@Json(name = "firstPrzwnerCo")
+	val firstPrizeWinnerCount: Int = INVALID,
+	@Json(name = "firstAccumamnt")
+	val firstAccumulatedAmount: Long = INVALID.toLong(),
+	@Json(name = "drwtNo1")
+	val no1: Int = INVALID,
+	@Json(name = "drwtNo2")
+	val no2: Int = INVALID,
+	@Json(name = "drwtNo3")
+	val no3: Int = INVALID,
+	@Json(name = "drwtNo4")
+	val no4: Int = INVALID,
+	@Json(name = "drwtNo5")
+	val no5: Int = INVALID,
+	@Json(name = "drwtNo6")
+	val no6: Int = INVALID,
+	@Json(name = "bnusNo")
+	val bonusNo: Int = INVALID,
+	@Json(name = "drwNo")
+	val lotteryNo: Int = INVALID
+)
+
+fun NetworkLottoResult.isFail() = !isSuccess()
+
+fun NetworkLottoResult.isSuccess() = apiResult == SUCCESS_RESULT_STRING
+
+fun NetworkLottoResult.toDatabaseModel() = DatabaseLottoResult(
+	lotteryNo = lotteryNo,
+	day = dayOfLottery ?: "",
+	totalSellAmount = totalSellAmount,
+	firstWinAmount = firstWinAmount,
+	firstPrizeWinnerCount = firstPrizeWinnerCount,
+	firstAccumulatedAmount = firstAccumulatedAmount,
+	no1 = no1,
+	no2 = no2,
+	no3 = no3,
+	no4 = no4,
+	no5 = no5,
+	no6 = no6,
+	bonusNo = bonusNo,
+)
